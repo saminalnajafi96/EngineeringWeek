3df0d770cbfde81c9ed0dd9519c4077c580980e9
Samin Alnajafi <saminalnajafi96@gmail.com>
2018-04-13 16:18:29 +0100

    Displaying sessions

--- app/src/main/java/com/example/android/engineeringweek/SessionsActivity.java
+++ app/src/main/java/com/example/android/engineeringweek/SessionsActivity.java
@@ -1,34 +1,83 @@
 package com.example.android.engineeringweek;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CheckBox;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 
 public class SessionsActivity extends AppCompatActivity {
 
-    private ListView sessionsList;
+    CheckBox checkBox;
+    ExpandableListAdapter expandableListAdapter;
+    ExpandableListView expandableListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sessions);
 
-        sessionsList = (ListView) findViewById(R.id.sessionsListView);
-        final ArrayList<String> sessionNames = new ArrayList<>();
-        sessionNames.add("ONTAP Update");
-        sessionNames.add("Data Fabric");
-        sessionNames.add("NVMe-oF");
+        // Get the ListView
+        expandableListView = (ExpandableListView) findViewById(R.id.sessionsExpandableListView);
 
-        String[] listItems = new String[sessionNames.size()];
-        for(int i = 0; i < sessionNames.size(); i++) {
-            String s = sessionNames.get(i);
-            listItems[i] = s;
-        }
+        // Prepare the list data
+        prepareListData();
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
-        sessionsList.setAdapter(adapter);
+        expandableListAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // Set the list adapter and expand lists by default
+        expandableListView.setAdapter(expandableListAdapter);
+        expandableListView.expandGroup(0);
+
+        // Get checkbox values
+        checkBox = (CheckBox) findViewById(R.id.checkbox);
+    }
+
+    /**
+     * Prepare the list data
+     */
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Header data
+        listDataHeader.add("22nd May 2018");
+        listDataHeader.add("23rd May 2018");
+
+        // Child data
+        List<String> may22 = new ArrayList<>();
+        may22.add("09:00 - 10:00    ONTAP 9.4");
+        may22.add("10:00 - 11:00    Data Fabric");
+        may22.add("11:00 - 12:00    Snapcenter update");
+        may22.add("13:00 - 14:00    Cloud Data Services");
+        may22.add("14:00 - 15:00    HCI");
+        may22.add("15:00 - 16:00    NFLEX");
+        may22.add("16:00 - 17:00    Modernize your storage");
+        may22.add("17:00 - 17:30    Machine Learning & AI");
+
+        List<String> may23 = new ArrayList<>();
+        may23.add("09:00 - 10:00    ONTAP 9.4");
+        may23.add("10:00 - 11:00    Data Fabric");
+        may23.add("11:00 - 12:00    Snapcenter update");
+        may23.add("13:00 - 14:00    Cloud Data Services");
+        may23.add("14:00 - 15:00    HCI");
+        may23.add("15:00 - 16:00    NFLEX");
+        may23.add("16:00 - 17:00    Modernize your storage");
+        may23.add("17:00 - 17:30    Machine Learning & AI");
+
+        // Header, Child data
+        listDataChild.put(listDataHeader.get(0), may22);
+        listDataChild.put(listDataHeader.get(1), may23);
     }
 }
