8ac696905c46ec07bd9647bbeb257dd840f14f10
elliotw <elliotwilde@outlook.com>
2018-05-08 16:27:49 +0100

    Created splash screen

--- app/src/androidTest/java/com/netapp/android/engineeringweek/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/netapp/android/engineeringweek/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.android.engineeringweek", appContext.getPackageName());
+        assertEquals("com.netapp.android.engineeringweek", appContext.getPackageName());
     }
 }
